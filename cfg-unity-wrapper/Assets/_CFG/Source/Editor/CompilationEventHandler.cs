8eaa3873649c074d6bfbe0da4aa1a4d62e806d78
Dmitry Melkov <melkov@itibsoft.ru>
2024-10-29 11:28:34 +0700

    - feat: create logic for event compilation

---
+++ cfg-unity-wrapper/Assets/_CFG/Source/Editor/CompilationEventHandler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace CFG
+{
+    [InitializeOnLoad]
+    public class CompilationEventHandler
+    {
+        static CompilationEventHandler()
+        {
+            AssemblyReloadEvents.beforeAssemblyReload += OnBeforeAssemblyReload; 
+            AssemblyReloadEvents.afterAssemblyReload += OnAfterAssemblyReload; 
+        }
+
+        private static void OnBeforeAssemblyReload()
+        {
+            Debug.Log("Перед перекомпиляцией");
+        }
+
+        private static void OnAfterAssemblyReload()
+        {
+            Debug.Log("После перекомпиляции");
+        }
+    }
+}
+
+
